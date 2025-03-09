92539cac933c82c7e1bedd5784854b53527504a4
Mehdi <medghouly@gmail.com>
2025-03-09 13:38:40 +0100

    feat: implement Tailwind CSS styling and add Navbar and Dashboard components

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, DashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular';
-}
+export class AppComponent {}
